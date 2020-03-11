7ac52024525565cf6e478ce738a5af2d61e325ef
Bohdan Solovey <hondadiozx72@gmail.com>
2020-03-11 23:00:09 +0200

    logo addon

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 var bigBall = document.querySelector('.cursor-big');
 var smallBall = document.querySelector('.cursor-small');
 var hoverables = document.querySelectorAll('.hoverable');
-var photoHover = document.getElementById('hoverable-photo');
-var hoverPhotoText = document.getElementById('hoverable-photo-text');
 
 // Listeners
 document.body.addEventListener('mousemove', onMouseMove);
 for (var i = 0; i < hoverables.length; i++) {
 	hoverables[i].addEventListener('mouseenter', onMouseHover);
 	hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
-	hoverPhotoText.addEventListener('mouseenter', onMouseImageHover);
-	hoverPhotoText.addEventListener('mouseleave', onMouseImageHoverOut);
 }
 
 // Move the cursor
@@ -23,10 +19,6 @@ function onMouseMove(e) {
 		x: e.pageX - 5,
 		y: e.pageY - 7
 	});
-	TweenMax.to(photoHover, 0.1, {
-		x: e.pageX - 500,
-		y: e.pageY - 200
-	});
 }
 
 // Hover an element
@@ -38,18 +30,5 @@ function onMouseHover() {
 function onMouseHoverOut() {
 	TweenMax.to(bigBall, 0.3, {
 		scale: 1
-	});
-}
-// Hover an Bohdan Solovey text
-function onMouseImageHover() {
-	TweenMax.to(photoHover, 0.3, {
-		scale: 1.5,
-		opacity: 1
-	});
-}
-function onMouseImageHoverOut() {
-	TweenMax.to(photoHover, 0.3, {
-		scale: 1,
-		opacity: 0
 	});
 }
